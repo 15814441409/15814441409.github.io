c91dddebd07daf9770dfcc9abee1ae55baa198fa
nihao <390975774@qq.com>
2017-06-22 08:55:27 +0800

    6月21日提交

---
+++ js/addres.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Administrator
+* @Date:   2017-06-21 15:07:23
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-06-21 15:09:19
+*/
+
+'use strict';
+function addres() {
+	var addres = document.querySelector('#addres');
+}
